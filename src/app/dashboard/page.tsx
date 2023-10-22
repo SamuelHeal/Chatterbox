e41354954a4d17955e354c77ccf44d69de942170
Samuel Heal <sam@Samuels-MacBook-Air.local>
2023-10-22 17:32:01 +1030

    auth-callback setup - actual download for new projects here. Make sure to change / add .env file and change schema where needed

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,24 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
+import { db } from "@/db";
+import Dashboard from "@/components/Dashboard";
 
-function Page() {
+async function Page() {
   const { getUser } = getKindeServerSession();
 
   const user = getUser();
 
   if (!user || !user.id) redirect("/auth-callback?origin=dashboard");
 
-  return <div>{user.email}</div>;
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (!dbUser) redirect("/auth-callback?origin=dashboard");
+
+  return <Dashboard />;
 }
 
 export default Page;
---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+  return <main className='mx-auto m-x-7xl md:p-10'>Dashboard</main>;
+}
+
+export default Dashboard;
